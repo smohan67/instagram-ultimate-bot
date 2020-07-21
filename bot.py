e3e65dc2f20f6163bc1284416b50b111c213a899
smohan67 <61168189+smohan67@users.noreply.github.com>
2020-07-21 00:36:38 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
+import urllib.request
 #import all the selenium packages
 
 
